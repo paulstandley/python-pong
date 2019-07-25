88feaef02fd160a7b42997a78649960767306127
Paul Standley <paulstandley1972@gmail.com>
2019-07-25 21:41:41 +0100

    make paddles move

--- Pong.py
+++ Pong.py
@@ -1,5 +1,4 @@
-"""This is pong"""
-
+"""This is pong""" 
 # pong
 # setup
 import turtle
@@ -11,30 +10,64 @@ wn.setup(width=800, height=600)
 wn.tracer(0) 
 
 #  Paddel a
-paddel_a = turtle.Turtle()
-paddel_a.speed(0)
-paddel_a.shape('square')
-paddel_a.color('white')
-paddel_a.shapesize(stretch_wid = 5, stretch_len = 1)
-paddel_a.penup()
-paddel_a.goto(-350, 0)
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape('square')
+paddle_a.color('white')
+paddle_a.shapesize(stretch_wid = 5, stretch_len = 1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
 
 # Paddel b
-paddel_a = turtle.Turtle()
-paddel_a.speed(0)
-paddel_a.shape('square')
-paddel_a.color('white')
-paddel_a.shapesize(stretch_wid = 5, stretch_len = 1)
-paddel_a.penup()
-paddel_a.goto(350, 0)
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape('square')
+paddle_b.color('white')
+paddle_b.shapesize(stretch_wid = 5, stretch_len = 1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
 
 # Ball
-ball_a = turtle.Turtle()
-ball_a.speed(0)
-ball_a.shape('square')
-ball_a.color('white')
-ball_a.penup()
-ball_a.goto(0, 0)
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape('square')
+ball.color('white')
+ball.penup()
+ball.goto(0, 0)
+
+# function
+def paddle_a_up():
+	"""Paddle a up"""
+	y = paddle_a.ycor()
+	y += 20
+	paddle_a.sety(y)
+ 
+def paddle_a_down():
+	"""Paddle a down"""
+	y = paddle_a.ycor()
+	y += -20
+	paddle_a.sety(y)
+
+def paddle_b_up():
+	"""Paddle b up"""
+	y = paddle_b.ycor()
+	y += 20
+	paddle_b.sety(y)
+ 
+def paddle_b_down():
+	"""Paddle b down"""
+	y = paddle_b.ycor()
+	y += -20
+	paddle_b.sety(y)
+ 
+  
+# ketbord binding
+wn.listen()
+wn.onkeypress(paddle_a_up, 'w')
+wn.onkeypress(paddle_a_down, 's')
+wn.onkeypress(paddle_b_up, 'Up')
+wn.onkeypress(paddle_b_down, 'Down')
+
 
 # Main game loop
 while True:
