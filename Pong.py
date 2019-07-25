7b87d5f876820199f248f285a12214a6574e7ca3
Paul Standley <paulstandley1972@gmail.com>
2019-07-25 18:17:18 +0100

    add paddel b

--- Pong.py
+++ Pong.py
@@ -20,6 +20,13 @@ paddel_a.penup()
 paddel_a.goto(-350, 0)
 
 # Paddel b
+paddel_a = turtle.Turtle()
+paddel_a.speed(0)
+paddel_a.shape('square')
+paddel_a.color('white')
+paddel_a.shapesize(stretch_wid = 5, stretch_len = 1)
+paddel_a.penup()
+paddel_a.goto(350, 0)
 
 # Ball
 
