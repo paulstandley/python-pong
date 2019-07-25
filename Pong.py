1665e6042a642d0afccd195a11ca9fa4bc64b83c
Paul Standley <paulstandley1972@gmail.com>
2019-07-25 18:14:23 +0100

    add paddel a

--- Pong.py
+++ Pong.py
@@ -1,3 +1,5 @@
+"""This is pong"""
+
 # pong
 # setup
 import turtle
@@ -6,7 +8,20 @@ wn = turtle.Screen()
 wn.title('pong by @TokyoEdTech')
 wn.bgcolor('black')
 wn.setup(width=800, height=600)
-wn.tracer(0)
+wn.tracer(0) 
+
+#  Paddel a
+paddel_a = turtle.Turtle()
+paddel_a.speed(0)
+paddel_a.shape('square')
+paddel_a.color('white')
+paddel_a.shapesize(stretch_wid = 5, stretch_len = 1)
+paddel_a.penup()
+paddel_a.goto(-350, 0)
+
+# Paddel b
+
+# Ball
 
 # Main game loop
 while True:
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Paul Standley
+
+[Pong](https://www.youtube.com/watch?v=C6jJg9Zan7w&t=102s)
